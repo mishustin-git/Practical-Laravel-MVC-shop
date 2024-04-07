20c48e26d4d4b4064e194b97a47b8aef4fd95516
mishustin-git <mishustin.post@yandex.ru>
2024-04-07 13:57:27 +0300

    clean mvc web application backup

---
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminHomeController extends Controller{
+    public function index(){
+        $viewData = [];
+        $viewData['title'] = "Admin page - admin online store";
+        return view('admin.home.index')->with('viewData', $viewData);
+    }
+}
