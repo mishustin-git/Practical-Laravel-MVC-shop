142f3785d852464be3b01698a020a5daec235d6a
mishustin-git <mishustin.post@yandex.ru>
2024-04-07 14:07:57 +0300

    Product Purchase

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 
-Route::get('/','App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
-Route::get('/products','App\Http\Controllers\ProductController@index')->name('product.index');
-Route::get('/products/{id}','App\Http\Controllers\ProductController@show')->name('product.show');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
 
-Route::get('/cart', [ CartController::class, 'index' ])->name("cart.index");
-Route::get('/cart/delete', [ CartController::class, 'delete' ])->name("cart.delete");
-Route::post('/cart/add/{id}', [ CartController::class, 'add' ])->name("cart.add");
+Route::get('/cart', [CartController::class, 'index'])->name("cart.index");
+Route::get('/cart/delete', [CartController::class, 'delete'])->name("cart.delete");
+Route::post('/cart/add/{id}', [CartController::class, 'add'])->name("cart.add");
 
-Route::middleware('admin')->group(function(){
+Route::middleware('auth')->group(function () {
+    Route::get('/cart/purchase', [CartController::class, 'purchase'])->name("cart.purchase");
+});
+
+Route::middleware('admin')->group(function () {
     Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
     Route::get('admin/product', 'App\Http\Controllers\Admin\AdminProductController@index')->name('admin.product.index');
     Route::post('admin/prodcts/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name('admin.product.store');
